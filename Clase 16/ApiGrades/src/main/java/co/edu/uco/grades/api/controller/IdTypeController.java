d3d2b27da433b034095514d9e3a6a606c8292cf7
Dante crybaby <100450786+DavidPerazaUwU@users.noreply.github.com>
2022-05-28 04:51:35 -0500

    Update IdTypeController.java

--- Clase 16/ApiGrades/src/main/java/co/edu/uco/grades/api/controller/IdTypeController.java
+++ Clase 16/ApiGrades/src/main/java/co/edu/uco/grades/api/controller/IdTypeController.java
@@ -9,17 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/idtype")
-public class IdTypeController {
+public class IDTypeController {
 	
 	@GetMapping("/dummy")
 	public IdTypeDTO getDummy() {
 		return new IdTypeDTO();
 	}
 	
-	@PostMapping 
-	public ResponseEntity<Response<IdTypeDTO>> create(@RequestBody IdTypeDTO dto){
+	@PostMapping
+	public ResponseEntity<Response<IdType >>
 		
-	
 		Validator<IdTypeDTO> validator = new CreateIdTypeValidator();
 		List<String> messages = UtilObject.getUtilObject().getDefault(validator.validate(dto), new ArrayList<>());
 		Response<IdTypeDTO> response = new Response<>();
@@ -35,32 +34,31 @@ public class IdTypeController {
 			}catch (GradesExpection exception) {
 				if(ExceptionType.TECHNICAL.equals(exception.getType())) {
 					messages.add("There was a problem trying to registres the new id Type. Please try again"){
-					System.err.println(exception.getLocation());
-					System.err.println(exception.getType());
-					System.err.println(exception.getTechnicalMessage());
+					System.err.println(exception.getLocation()));
+					System.err.println(exception.getType()));
+					System.err.println(exception.getMessage()));
 					exception.getRootException().printSackTrace();
-				} else {
-					messages.add(exception.getUserMessage());
-					System.err.println(exception.getLocation());
-					System.err.println(exception.getType());
-					System.err.println(exception.getUserMessage());
+				}else {
+					messages.add
+					System.err.println(exception.getType()));
+					System.err.println(exception.getMessage()));
 					exception.getRootException().printSackTrace();		
 			}	
 		}catch (Exception exception) {	
 			messages.add("there was an unexpeced problem trying to registrer the new id Type");
 			exception.printStackTrace();
 			}
-		}
+		
+		
+				
+	}}
 	
 	response.setMessage(messages);
-	responseEntity = new ResponseEntity<>(response, statusCode);
-	
-	return responseEntity;
- }
+	responseEntity = new ResponseEntity<>(response,statusCode);
 
 	
 	@PutMapping("/{id}")
-	public void update(@PathVariable("id") int id, @RequestBody IdTypeDTO dto) {
+	public void update(@PathVariable("id") int id,@RequestBody dTypDTO dto) {
 		System.out.println("Estoy en actualizar!");
 	}
 	
@@ -74,42 +72,8 @@ public class IdTypeController {
 		System.out.println("Estoy en consultar por id!");
 	}
 	@GetMapping("/{id}")
-	public ResponseEntity<Response<IdTypeDTO>> find() {
-		List<String> messages =  new ArrayList<>());
-		Response<IdTypeDTO> response = new Response<>();
-		ResponseEntity<Response>> resonseEntity;
-		HttpStatus statusCode = HttpStatus.BAD_REQUEST;
-		
-		
-			try {
-				IdTypeFacade facade = new IdTypeFacadeImpl();
-				reponse.setData(facade.find(getDummy(new IdTypeDTO)));
-				messages.add("Id type were found succesfully!");
-				statusCode = HttpStatus.OK;
-			}catch (GradesExpection exception) {
-				if(ExceptionType.TECHNICAL.equals(exception.getType())) {
-					messages.add("There was a problem trying to find id Type. Please try again");
-					System.err.println(exception.getLocation());
-					System.err.println(exception.getType());
-					System.err.println(exception.getTechnicalMessage());
-					exception.getRootException().printSackTrace();
-				} else {
-					messages.add(exception.getUserMessage());
-					System.err.println(exception.getLocation());
-					System.err.println(exception.getType());
-					System.err.println(exception.getUserMessage());
-					exception.getRootException().printSackTrace();		
-			}	
-		}catch (Exception exception) {	
-			messages.add("there was an unexpeced problem trying to find id Type");
-			exception.printStackTrace();
-			}
-		
-	
-	response.setMessage(messages);
-	responseEntity = new ResponseEntity<>(response, statusCode);
-	
-	return responseEntity;
+	public void fin() {
+		System.out.println("Estoy en consultar todos!");
 	}	
 
 }
